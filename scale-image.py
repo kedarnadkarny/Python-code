29b20d894388bb0bf257a3b7746f5dd5c92e6af7
Kedar Nadkarny <kedar.nadkarny@gmail.com>
2016-07-12 17:47:37 +0530

    Update scale-image.py

--- scale-image.py
+++ scale-image.py
@@ -1,14 +1,14 @@
 from PIL import Image
 import os
 
-list = os.listdir("C:\\wamp\\www\\ajantamarbles\images\products\granite-samples-2") # dir is your directory path
+list = os.listdir("path") # dir is your directory path
 number_files = len(list)
 print(number_files)
 
 size = 150, 150
 
-for filename in os.listdir("C:\\wamp\\www\\ajantamarbles\images\products\granite-samples-2"):
-    oldImage = Image.open("C:\\wamp\\www\\ajantamarbles\images\products\granite-samples-2\%s" % filename)
-    print("C:\\wamp\\www\\ajantamarbles\images\products\granite-samples-2\%s" % filename)
+for filename in os.listdir("path"):
+    oldImage = Image.open("path\%s" % filename)
+    print("path\%s" % filename)
     newImage = oldImage.resize(size, Image.ANTIALIAS)
-    newImage.save("C:\wamp\www\\ajantamarbles\images\products\\thumbnail\%s" % filename)
+    newImage.save("newpath\%s" % filename)
